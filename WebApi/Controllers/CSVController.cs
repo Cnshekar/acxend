ca7af337d645d3c563f62d61cd7fbf66211b5e49
Cnshekar <Cnshekar123@gmail.com>
2021-03-18 19:50:30 +0530

    initial Commit

---
+++ WebApi/Controllers/CSVController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+
+namespace WebApi.Controllers
+{
+    public class CSVController : Controller
+    {
+        // (CSV/SaveData)
+        public ActionResult SaveData([FromBody] StudentViewModel student)
+        {
+            if (!ModelState.IsValid)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            using (var ctx = new SchoolDBEntities())
+            {
+                ctx.Students.Add(new Student()
+                {
+                    StudentID = student.Id,
+                    Name = student.Name,
+                    Age = student.Age,
+                    City = student.City
+                });
+
+                ctx.SaveChanges();
+            }
+
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK, "Successfully Stored");
+
+        }
+    }
+}
